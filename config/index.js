1f51e00ed1766d8d2199a35ac9493a279bd9d53c
Echoviolet <1144608711@qq.com>
2017-05-24 18:31:06 +0800

    something

--- config/index.js
+++ config/index.js
@@ -1,3 +1,27 @@
 /**
- * Created by Administrator on 2017/5/24.
+ * Created by Echo on 2017/5/24.
  */
+var path = require('path');
+
+module.export = {
+    build: {
+        env: require('./prod.env'),
+        index: path.resolve(__dirname,'../dist/index.html'),
+        assetsRoot: path.resolve(__dirname,'../dist'),
+        assetsSubDirectory:'./static',
+        assetsPublicPath: './',
+        productionSourceMap: true,
+        productionGzip: false,
+        productionGzipExtensions: ['js','css'],
+
+    },
+    dev: {
+        env: require('./dev.env'),
+        port:8090,
+        assetsSubDirectory: 'static',
+        assetsPublicPath: '/',
+        proxyTable: {},
+        cssSourceMap: false
+
+    }
+}
--- src/common/js/util.js
+++ src/common/js/util.js
@@ -1,3 +1,21 @@
 /**
- * Created by Administrator on 2017/5/24.
+ * 解析url参数
+ * Created by Echo on 2017/5/24.
+ * @return object {id:12334}
  */
+export function urlParse() {
+    let url = window.location.search;
+    let obj = {};
+    let reg = /[?&][^?&]+=[^?&]]+/g/;
+    let arr = url.match(reg);
+
+    if(arr){
+        arr.forEach((item) => {
+            let tempArr = item.substring(1).split('=');
+            let key = tempArr[0];
+            let val = tempArr[1];
+            obj[key] = val;
+        });
+    }
+    return { id: 123123 };
+};
